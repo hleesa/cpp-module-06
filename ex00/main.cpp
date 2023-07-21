1028ea1b60cf5f877c00ce57ccb675344d4215d5
salee2 <hleesa>
2023-07-21 14:12:07 +0900

    delete comment

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,10 +6,6 @@ int main(int argc, char* argv[]) {
 		std::cout << "No input provided\n";
 		return 0;
 	}
-//	float f = 42.99;
-//	std::cout << f << '\n';
-//	std::cout << LONG_MAX << '\n';
-//	std::cout << INT_MAX << '\n';
 	ScalarConverter::convert(argv[1]);
 	return 0;
 }
